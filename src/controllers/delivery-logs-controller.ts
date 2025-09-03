9bba6aa44cfaa1590893c1e980db78d71cc1c93f
Monique Dias <moniquedbandas@gmail.com>
2025-09-03 17:54:09 -0300

    fix: show delivery not found case not exist

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,31 +1,31 @@
-import { Request, Response } from "express"
-import { AppError } from "@/utils/AppError"
-import { prisma } from "@/database/prisma"
-import { z } from "zod"
+import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
 
 class DeliveryLogsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       delivery_id: z.string().uuid(),
       description: z.string(),
-    })
+    });
 
-    const { delivery_id, description } = bodySchema.parse(request.body)
+    const { delivery_id, description } = bodySchema.parse(request.body);
 
     const delivery = await prisma.delivery.findUnique({
       where: { id: delivery_id },
-    })
+    });
 
     if (!delivery) {
-      throw new AppError("delivery not found", 404)
+      throw new AppError("delivery not found", 404);
     }
 
     if (delivery.status === "delivered") {
-      throw new AppError("this order has already been delivered")
+      throw new AppError("this order has already been delivered");
     }
 
     if (delivery.status === "processing") {
-      throw new AppError("change status to shipped")
+      throw new AppError("change status to shipped");
     }
 
     await prisma.deliveryLog.create({
@@ -33,17 +33,17 @@ class DeliveryLogsController {
         deliveryId: delivery_id,
         description,
       },
-    })
+    });
 
-    return response.status(201).json()
+    return response.status(201).json();
   }
 
   async show(request: Request, response: Response) {
     const paramsSchema = z.object({
       delivery_id: z.string().uuid(),
-    })
+    });
 
-    const { delivery_id } = paramsSchema.parse(request.params)
+    const { delivery_id } = paramsSchema.parse(request.params);
 
     const delivery = await prisma.delivery.findUnique({
       where: { id: delivery_id },
@@ -51,17 +51,19 @@ class DeliveryLogsController {
         user: true,
         logs: true,
       },
-    })
-
+    });
+    if (!delivery) {
+      return response.status(404).json({ message: "delivery not found" });
+    }
     if (
       request.user?.role === "customer" &&
       request.user.id !== delivery?.userId
     ) {
-      throw new AppError("the user can only view their deliveries", 401)
+      throw new AppError("the user can only view their deliveries", 401);
     }
 
-    return response.json(delivery)
+    return response.json(delivery);
   }
 }
 
-export { DeliveryLogsController }
+export { DeliveryLogsController };
